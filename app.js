df724ba870ed1e6ec5fb5802bdf27e1c33de7c39
Omar Alhamad <omar93@hotmail.se>
2024-12-02 13:57:04 +0100

    rest of images etc

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.use(express.static('src/public'))
+app.use(express.static('./svelte/dist/public'))
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
